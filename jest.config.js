e671bfdafd0a8b408caced02cdee7ec23a9e73e8
Marco Lavielle <marcoalfonso@gmail.com>
2024-04-18 13:14:05 +1000

    feature - add jest configuration

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/tests/__mocks__/fileMock.js',
+  },
+};
